d30ccfb774c464f0e152cce38ee7c20cb69e8fb9
Tanvir Bhuiyan <tanvirbhuiyann@gmail.com>
2023-11-01 02:32:39 +0600

    User module dome

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -50,6 +50,7 @@ class StateResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
